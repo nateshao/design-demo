22e1dab699dcce38aba9957d3961106631678dcf
nateshao <shaotongjie@gmail.com>
2020-12-08 16:56:30 +0800

    init

--- nateshao-design-demo-1-02/src/test/java/com/nateshao/design/demo/NateshaoDesignDemo102ApplicationTests.java
+++ nateshao-design-demo-1-02/src/test/java/com/nateshao/design/demo/NateshaoDesignDemo102ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NateshaoDesignDemo102ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
