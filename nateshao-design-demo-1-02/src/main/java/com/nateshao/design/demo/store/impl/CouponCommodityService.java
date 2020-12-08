b74a8273dddad4f0a732990098db2cd965885fd5
nateshao <shaotongjie@gmail.com>
2020-12-08 19:38:21 +0800

    init

--- nateshao-design-demo-1-02/src/main/java/com/nateshao/design/demo/store/impl/CouponCommodityService.java
+++ nateshao-design-demo-1-02/src/main/java/com/nateshao/design/demo/store/impl/CouponCommodityService.java
@@ -1,5 +1,12 @@
 package com.nateshao.design.demo.store.impl;
 
+import com.nateshao.design.demo.coupon.CouponService;
+import com.nateshao.design.demo.store.ICommodity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
 /**
  * @date Created by 邵桐杰 on 2020/12/8 19:31
  * @微信公众号 千羽的编程时光
@@ -9,5 +16,15 @@ package com.nateshao.design.demo.store.impl;
  * @Gitee https://gitee.com/nateshao
  * Description:
  */
-public class CouponCommodityService {
+public class CouponCommodityService implements ICommodity {
+
+    private Logger logger =
+            LoggerFactory.getLogger(CouponCommodityService.class);
+
+    private CouponService couponService = new CouponService();
+
+    @Override
+    public void sendCommodity(String uId, String commodityId, String bizId, Map<String, String> extMap) throws Exception {
+
+    }
 }
