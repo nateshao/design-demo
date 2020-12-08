641ac70e71920682ac3b266df793c62500af34fc
nateshao <shaotongjie@gmail.com>
2020-12-08 23:43:19 +0800

    abstract-factory

--- nateshao-design-demo-2-00-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/RedisUtils.java
+++ nateshao-design-demo-2-00-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/RedisUtils.java
@@ -14,7 +14,9 @@ import java.util.concurrent.TimeUnit;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 模拟单机服务 RedisUtils
+ *              模拟Redis功能，也就是假定⽬前所有的系统都在使⽤的服务
+ *              类和⽅法名次都固定写死到各个业务系统中，改动略微麻烦
  */
 public class RedisUtils {
     private Logger logger = LoggerFactory.getLogger(RedisUtils.class);
--- nateshao-design-demo-2-00-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/cuisine/impl/CacheServiceImpl.java
+++ nateshao-design-demo-2-00-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/cuisine/impl/CacheServiceImpl.java
@@ -1,5 +1,10 @@
 package com.nateshao.design.abstractfactory.cuisine.impl;
 
+import com.nateshao.design.abstractfactory.CacheService;
+import com.nateshao.design.abstractfactory.RedisUtils;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @date Created by 邵桐杰 on 2020/12/8 23:40
  * @微信公众号 千羽的编程时光
@@ -9,5 +14,26 @@ package com.nateshao.design.abstractfactory.cuisine.impl;
  * @Gitee https://gitee.com/nateshao
  * Description:
  */
-public class CacheServiceImpl {
+public class CacheServiceImpl implements CacheService {
+
+    private RedisUtils redisUtils = new RedisUtils();
+
+    @Override
+    public String get(String key) {
+        return redisUtils.get(key);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        redisUtils.set(key, value);
+    }
+    @Override
+    public void set(String key, String value, long timeout, TimeUnit timeUnit) {
+        redisUtils.set(key, value, timeout, timeUnit);
+    }
+    @Override
+    public void del(String key) {
+        redisUtils.del(key);
+    }
+
 }
