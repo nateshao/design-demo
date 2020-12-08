a5fd83e61f9af5b637542f1c008e8cefd5956447
nateshao <shaotongjie@gmail.com>
2020-12-08 17:28:38 +0800

    init

---
+++ nateshao-design-demo-1-00/src/main/java/com/nateshao/design/demo/goods/DeliverReq.java
@@ -0,0 +1,76 @@
+package com.nateshao.design.demo.goods;
+
+/**
+ * @date Created by 邵桐杰 on 2020/12/8 17:21
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class DeliverReq {
+    private String userName;              // 用户姓名
+    private String userPhone;             // 用户手机
+    private String sku;                   // 商品SKU
+    private String orderId;               // 订单ID
+    private String consigneeUserName;     // 收货人姓名
+    private String consigneeUserPhone;    // 收货人手机
+    private String consigneeUserAddress;  // 收获人地址
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getConsigneeUserName() {
+        return consigneeUserName;
+    }
+
+    public void setConsigneeUserName(String consigneeUserName) {
+        this.consigneeUserName = consigneeUserName;
+    }
+
+    public String getConsigneeUserPhone() {
+        return consigneeUserPhone;
+    }
+
+    public void setConsigneeUserPhone(String consigneeUserPhone) {
+        this.consigneeUserPhone = consigneeUserPhone;
+    }
+
+    public String getConsigneeUserAddress() {
+        return consigneeUserAddress;
+    }
+
+    public void setConsigneeUserAddress(String consigneeUserAddress) {
+        this.consigneeUserAddress = consigneeUserAddress;
+    }
+}
