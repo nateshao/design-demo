676f67ca8e6d0c9f41d7e5bca916a6513425e1bf
nateshao <shaotongjie@gmail.com>
2020-12-08 16:28:57 +0800

    init

---
+++ nateshao-design-demo-1-01/src/main/java/com/nateshao/design/nateshaodesigndemo101/NateshaoDesignDemo101Application.java
@@ -0,0 +1,13 @@
+package com.nateshao.design.nateshaodesigndemo101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoDesignDemo101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoDesignDemo101Application.class, args);
+    }
+
+}
