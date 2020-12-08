612464e3cd04364d93624ae5735b77ce0cc9e1f2
nateshao <shaotongjie@gmail.com>
2020-12-08 16:00:23 +0800

    init

---
+++ nateshao-design-demo-1-00/src/main/java/com/nateshao/design/demo/NateshaoDesignDemo100Application.java
@@ -0,0 +1,13 @@
+package com.nateshao.design.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoDesignDemo100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoDesignDemo100Application.class, args);
+    }
+
+}
---
+++ nateshao-design-demo-1-00/src/test/java/com/nateshao/design/demo/NateshaoDesignDemo100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.design.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoDesignDemo100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
