5956e8c65ded21da4fa8e6bb0d248166c5d03e07
nateshao <shaotongjie@gmail.com>
2020-12-08 19:08:55 +0800

    init

--- nateshao-design-demo-1-01/src/test/java/com/nateshao/design/nateshaodesigndemo101/PrizeControllerTest.java
+++ nateshao-design-demo-1-01/src/test/java/com/nateshao/design/nateshaodesigndemo101/PrizeControllerTest.java
@@ -1,5 +1,9 @@
 package com.nateshao.design.nateshaodesigndemo101;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +17,19 @@ import static org.junit.Assert.*;
  */
 public class PrizeControllerTest {
 
+    private Logger logger = LoggerFactory.getLogger(PrizeControllerTest.class);
+
+    @Test
+    public void test_awardToUser(){
+        PrizeController prizeController = new PrizeController();
+        System.out.println("\\r\\n模拟发放优惠券测试\\r\\n");
+        // 模拟发放优惠券测试
+        AwardReq req01 = new AwardReq();
+        req01.setuId("10001");
+        req01.setAwardType(1);
+        req01.setAwardNumber("EGM1023938910232121323432");
+        req01.setBizId("791098764902132");
+
+    }
 
 }
