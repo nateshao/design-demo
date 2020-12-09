8651e430a095f020f090f6e044f82dbc9cbe30fc
nateshao <shaotongjie@gmail.com>
2020-12-09 09:02:02 +0800

    abstract-factory

---
+++ nateshao-design-demo-2-02-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/JDKProxy.java
@@ -0,0 +1,13 @@
+package com.nateshao.design.abstractfactory;
+
+/**
+ * @date Created by 邵桐杰 on 2020/12/9 9:00
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class JDKProxy {
+}
