cd34181fb76acf10d2a89a2d34ce9e6811adb7ae
nateshao <shaotongjie@gmail.com>
2020-12-09 20:09:26 +0800

    abstract-factory

--- 3-00-builder/src/main/java/nateshao/design/demo/builder/floor/DerFloor.java
+++ 3-00-builder/src/main/java/nateshao/design/demo/builder/floor/DerFloor.java
@@ -1,13 +1,44 @@
 package nateshao.design.demo.builder.floor;
 
+import nateshao.design.demo.builder.Matter;
+
+import java.math.BigDecimal;
+
 /**
+ * @author 邵桐杰
  * @date Created by 邵桐杰 on 2020/12/9 17:31
  * @微信公众号 千羽的编程时光
  * @个人网站 www.nateshao.cn
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 地板(floor)
+ *                  德尔
  */
-public class DerFloor {
+public class DerFloor implements Matter {
+
+    @Override
+    public String scene() {
+        return "地板";
+    }
+
+    @Override
+    public String brand() {
+        return "德尔(Der)";
+    }
+
+    @Override
+    public String model() {
+        return "A+";
+    }
+
+    @Override
+    public BigDecimal price() {
+        return new BigDecimal(119);
+    }
+
+    @Override
+    public String desc() {
+        return "DER德尔集团是全球领先的专业木地板制造商，北京2008年奥运会家装和公装地板供应商";
+    }
 }
