6f0d5e216e77bc64341b27059fd5f7c5dc002e64
nateshao <shaotongjie@gmail.com>
2020-12-09 23:28:19 +0800

    抽象工厂

--- 2-02-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/impl/IIRCacheAdapter.java
+++ 2-02-abstract-factory/src/main/java/com/nateshao/design/abstractfactory/impl/IIRCacheAdapter.java
@@ -18,18 +18,22 @@ public class IIRCacheAdapter implements ICacheAdapter {
 
     private IIR iir = new IIR();
 
+    @Override
     public String get(String key) {
         return iir.get(key);
     }
 
+    @Override
     public void set(String key, String value) {
         iir.set(key, value);
     }
 
+    @Override
     public void set(String key, String value, long timeout, TimeUnit timeUnit) {
         iir.setExpire(key, value, timeout, timeUnit);
     }
 
+    @Override
     public void del(String key) {
         iir.del(key);
     }
