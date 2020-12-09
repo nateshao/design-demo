def4742b997c712e2b3e55056ce935b7a6f0dcc2
nateshao <shaotongjie@gmail.com>
2020-12-09 20:07:17 +0800

    abstract-factory

--- 3-00-builder/src/main/java/nateshao/design/demo/builder/coat/LiBangCoat.java
+++ 3-00-builder/src/main/java/nateshao/design/demo/builder/coat/LiBangCoat.java
@@ -1,5 +1,9 @@
 package nateshao.design.demo.builder.coat;
 
+import nateshao.design.demo.builder.Matter;
+
+import java.math.BigDecimal;
+
 /**
  * @date Created by 邵桐杰 on 2020/12/9 17:31
  * @微信公众号 千羽的编程时光
@@ -7,7 +11,32 @@ package nateshao.design.demo.builder.coat;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: ⽴邦
  */
-public class LiBangCoat {
+public class LiBangCoat implements Matter {
+
+    @Override
+    public String scene() {
+        return "涂料";
+    }
+
+    @Override
+    public String brand() {
+        return "⽴邦";
+    }
+
+    @Override
+    public String model() {
+        return "默认级别";
+    }
+
+    @Override
+    public BigDecimal price() {
+        return new BigDecimal(650);
+    }
+
+    @Override
+    public String desc() {
+        return "⽴邦始终以开发绿⾊产品、注᯿⾼科技、⾼品质为⽬标，以技术⼒量不断推进科 研和开发，满⾜消费者需求。";
+    }
 }
