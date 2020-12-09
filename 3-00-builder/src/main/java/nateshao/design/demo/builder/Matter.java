c646243b558d9533d133711b0d990003b4b51084
nateshao <shaotongjie@gmail.com>
2020-12-09 19:12:53 +0800

    abstract-factory

--- 3-00-builder/src/main/java/nateshao/design/demo/builder/Matter.java
+++ 3-00-builder/src/main/java/nateshao/design/demo/builder/Matter.java
@@ -1,5 +1,7 @@
 package nateshao.design.demo.builder;
 
+import java.math.BigDecimal;
+
 /**
  * @date Created by 邵桐杰 on 2020/12/9 17:33
  * @微信公众号 千羽的编程时光
@@ -7,7 +9,31 @@ package nateshao.design.demo.builder;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 装修物料
  */
-public class Matter {
+public interface Matter {
+    /**
+     * 场景；地板、地砖、涂料、吊顶
+     */
+    String scene();
+
+    /**
+     * 品牌
+     */
+    String brand();
+
+    /**
+     * 型号
+     */
+    String model();
+
+    /**
+     * 平米报价
+     */
+    BigDecimal price();
+
+    /**
+     * 描述
+     */
+    String desc();
 }
---
+++ 3-00-builder/src/main/java/nateshao/design/demo/builder/info.java
@@ -0,0 +1,18 @@
+package nateshao.design.demo.builder;
+
+/**
+ * @date Created by 邵桐杰 on 2020/12/9 19:12
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 装修材料
+ *              吊顶；一级、二级
+ *              涂料；多乐士(Dulux)、立邦
+ *              地砖(tile)；东鹏瓷砖、马可波罗(MARCO POLO)
+ *              地板(floor)；德尔(Der)、圣象
+ */
+public class info {
+
+}
