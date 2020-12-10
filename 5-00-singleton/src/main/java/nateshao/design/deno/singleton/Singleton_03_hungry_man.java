248bc36965db2a74c1df879350e7126ee9b57fcd
nateshao <shaotongjie@gmail.com>
2020-12-10 11:26:02 +0800

    原型模式

--- 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_03_hungry_man.java
+++ 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_03_hungry_man.java
@@ -8,6 +8,12 @@ package nateshao.design.deno.singleton;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  * Description:  饿汉模式(线程安全)
+ *
+ * 此种⽅式与我们开头的第⼀个实例化 Map 基本⼀致，在程序启动的时候直接运⾏加载，后续有外
+ * 部需要使⽤的时候获取即可。
+ * 但此种⽅式并不是懒加载，也就是说⽆论你程序中是否⽤到这样的类都会在程序启动之初进⾏创建。
+ * 那么这种⽅式导致的问题就像你下载个游戏软件，可能你游戏地图还没有打开呢，但是程序已经将
+ * 这些地图全部实例化。到你⼿机上最明显体验就⼀开游戏内存满了，⼿机卡了，需要换了。
  */
 public class Singleton_03_hungry_man {
 
