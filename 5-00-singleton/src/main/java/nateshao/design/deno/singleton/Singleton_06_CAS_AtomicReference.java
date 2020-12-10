fb62d0069f451d9652b4f89b608381aa9772296b
nateshao <shaotongjie@gmail.com>
2020-12-10 12:40:46 +0800

    Singleton_04_inner_class

--- 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_06_CAS_AtomicReference.java
+++ 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_06_CAS_AtomicReference.java
@@ -11,12 +11,10 @@ import java.util.concurrent.atomic.AtomicReference;
  * @Gitee https://gitee.com/nateshao
  * Description:  6. CAS「AtomicReference」(线程安全)
  *
- * java并发库提供了很多原⼦类来⽀持并发访问的数据安全
- * 性； AtomicInteger 、 AtomicBoolean 、 AtomicLong 、 AtomicReference 。
- * AtomicReference 可以封装引⽤⼀个V实例，⽀持并发访问如上的单例⽅式就是使⽤了这样的⼀个
- * 特点。
- * 使⽤CAS的好处就是不需要使⽤传统的加锁⽅式保证线程安全，⽽是依赖于CAS的忙等算法，依赖
- * 于底层硬件的实现，来保证线程安全。相对于其他锁的实现没有线程的切换和阻塞也就没有了额外
+ * java并发库提供了很多原⼦类来⽀持并发访问的数据安全性；AtomicInteger、AtomicBoolean、AtomicLong、AtomicReference 。
+ * AtomicReference 可以封装引⽤⼀个V实例，⽀持并发访问如上的单例⽅式就是使⽤了这样的⼀个特点。
+ * 使⽤CAS的好处就是不需要使⽤传统的加锁⽅式保证线程安全，⽽是依赖于CAS的忙等算法，
+ * 依赖于底层硬件的实现，来保证线程安全。相对于其他锁的实现没有线程的切换和阻塞也就没有了额外
  * 的开销，并且可以⽀持较⼤的并发性。
  * 当然CAS也有⼀个缺点就是忙等，如果⼀直没有获取到将会处于死循环中。
  */
@@ -43,5 +41,5 @@ public class Singleton_06_CAS_AtomicReference {
     public static void main(String[] args) {
         System.out.println(Singleton_06_CAS_AtomicReference.getInstance()); // org.itstack.demo.design.Singleton_06@2b193f2d
         System.out.println(Singleton_06_CAS_AtomicReference.getInstance()); // org.itstack.demo.design.Singleton_06@2b193f2d
-    }
+    } //Enumeration singleton
 }
