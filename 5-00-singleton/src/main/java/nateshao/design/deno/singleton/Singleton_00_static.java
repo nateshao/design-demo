7ebd39a2fdcd695b8110d9eedbcb1b4b08a43360
nateshao <shaotongjie@gmail.com>
2020-12-10 11:19:29 +0800

    原型模式

--- 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_00_static.java
+++ 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_00_static.java
@@ -11,7 +11,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  * Description: 7种单例模式实现
- *              -----0. 静态类使⽤ Lazy man
+ *              ----- 0. 静态类使⽤
+ * 以上这种⽅式在我们平常的业务开发中⾮常场常⻅，这样静态类的⽅式可以在第⼀次运⾏的时候直
+ * 接初始化Map类，同时这⾥我们也不需要到延迟加载在使⽤。
+ * 在不需要维持任何状态下，仅仅⽤于全局访问，这个使⽤使⽤静态类的⽅式更加⽅便。
+ * 但如果需要被继承以及需要维持⼀些特定状态的情况下，就适合使⽤单例模式。
  */
 public class Singleton_00_static {
 
--- 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_01_lazy_man.java
+++ 5-00-singleton/src/main/java/nateshao/design/deno/singleton/Singleton_01_lazy_man.java
@@ -7,7 +7,22 @@ package nateshao.design.deno.singleton;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 懒汉模式(线程不安全)
+ *
+ * 单例模式有⼀个特点就是不允许外部直接创建，也就是 new Singleton_01_lazy_man() ，因此这⾥在默认
+ * 的构造函数上添加了私有属性 private 。
+ * ⽬前此种⽅式的单例确实满⾜了懒加载，但是如果有多个访问者同时去获取对象实例你可以想象成
+ * ⼀堆⼈在抢厕所，就会造成多个同样的实例并存，从⽽没有达到单例的要求。
  */
 public class Singleton_01_lazy_man {
+    private static Singleton_01_lazy_man instance;
+    private Singleton_01_lazy_man() {
+    }
+
+    public static Singleton_01_lazy_man getInstance(){
+        if (null != instance) {
+            return instance;
+        }
+        return new Singleton_01_lazy_man();
+    }
 }
