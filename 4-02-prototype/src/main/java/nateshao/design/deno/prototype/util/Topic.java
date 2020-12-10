dfe77893078550f2ef93084bd3a12a8ed5f08316
nateshao <shaotongjie@gmail.com>
2020-12-10 10:52:02 +0800

    原型模式

--- 4-02-prototype/src/main/java/nateshao/design/deno/prototype/util/Topic.java
+++ 4-02-prototype/src/main/java/nateshao/design/deno/prototype/util/Topic.java
@@ -1,5 +1,7 @@
 package nateshao.design.deno.prototype.util;
 
+import java.util.Map;
+
 /**
  * @date Created by 邵桐杰 on 2020/12/10 10:30
  * @微信公众号 千羽的编程时光
@@ -10,4 +12,30 @@ package nateshao.design.deno.prototype.util;
  * Description:
  */
 public class Topic {
+    private Map<String, String> option;  // 选项；A、B、C、D
+    private String key;           // 答案；B
+
+    public Topic() {
+    }
+
+    public Topic(Map<String, String> option, String key) {
+        this.option = option;
+        this.key = key;
+    }
+
+    public Map<String, String> getOption() {
+        return option;
+    }
+
+    public void setOption(Map<String, String> option) {
+        this.option = option;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 }
