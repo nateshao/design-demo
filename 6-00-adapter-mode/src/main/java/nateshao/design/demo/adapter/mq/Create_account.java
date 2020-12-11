aaa31781d4e030e3b4540a8dc5bdefb2861af7fc
nateshao <shaotongjie@gmail.com>
2020-12-11 09:32:56 +0800

    代码优化

--- 6-00-adapter-mode/src/main/java/nateshao/design/demo/adapter/mq/Create_account.java
+++ 6-00-adapter-mode/src/main/java/nateshao/design/demo/adapter/mq/Create_account.java
@@ -1,5 +1,8 @@
 package nateshao.design.demo.adapter.mq;
 
+import com.alibaba.fastjson.JSON;
+import java.util.Date;
+
 /**
  * @date Created by 邵桐杰 on 2020/12/10 12:57
  * @微信公众号 千羽的编程时光
@@ -7,8 +10,49 @@ package nateshao.design.demo.adapter.mq;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 开户
  */
 public class Create_account {
+    private String number;      // 开户编号
+    private String address;     // 开户地
+    private Date accountDate;   // 开户时间
+    private String desc;        // 开户描述
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getAccountDate() {
+        return accountDate;
+    }
+
+    public void setAccountDate(Date accountDate) {
+        this.accountDate = accountDate;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
 
 }
