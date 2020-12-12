2424573397eda5ce61038520e983441b27e5cee1
nateshao <shaotongjie@gmail.com>
2020-12-12 12:22:58 +0800

    适配器模式

---
+++ 6-00-adapter-mode/src/main/java/nateshao/design/demo/adapter/mq/OrderMq.java
@@ -0,0 +1,57 @@
+package nateshao.design.demo.adapter.mq;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.Date;
+
+/**
+ * @date Created by 邵桐杰 on 2020/12/12 12:22
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class OrderMq {
+    private String uid;           // 用户ID
+    private String sku;           // 商品
+    private String orderId;       // 订单ID
+    private Date createOrderTime; // 下单时间
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getCreateOrderTime() {
+        return createOrderTime;
+    }
+
+    public void setCreateOrderTime(Date createOrderTime) {
+        this.createOrderTime = createOrderTime;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}
