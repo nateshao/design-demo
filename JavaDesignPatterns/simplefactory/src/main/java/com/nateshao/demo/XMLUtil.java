2bdac15c0514429667ecc69f7833f058783c6973
nateshao <shaotongjie@gmail.com>
2021-10-10 15:48:18 +0800

    Java设计模式

--- JavaDesignPatterns/simplefactory/src/main/java/com/nateshao/demo/XMLUtil.java
+++ JavaDesignPatterns/simplefactory/src/main/java/com/nateshao/demo/XMLUtil.java
@@ -3,6 +3,7 @@ package com.nateshao.demo;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
