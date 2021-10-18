5cfb17aedaf0633e86167433dbec1f1ebc86627c
nateshao <shaotongjie@gmail.com>
2021-10-18 23:25:11 +0800

    ssm

---
+++ JavaDesignPatterns/abstractfactory/src/main/java/com/nateshao/adapter/Client.java
@@ -0,0 +1,13 @@
+package com.nateshao.adapter;
+
+/**
+ * @date Created by 邵桐杰 on 2021/10/18 23:25
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Client {
+}
