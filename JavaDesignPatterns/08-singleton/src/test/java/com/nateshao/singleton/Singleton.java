e0f57e49d902b9c874217e0ce72d54c2003ca412
nateshao <shaotongjie@gmail.com>
2021-12-29 09:08:25 +0800

    设计模式

---
+++ JavaDesignPatterns/08-singleton/src/test/java/com/nateshao/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package com.nateshao.singleton;
+
+/**
+ * @date Created by 邵桐杰 on 2021/12/29 9:07
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Singleton {
+
+}
